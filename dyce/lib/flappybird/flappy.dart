5921ae98930279fe75a96b1d33d0724fb1d03f3f
brraymun <your_email@youremail.com>
2022-04-10 05:00:45 -0700

    idek

--- dyce/lib/flappybird/flappy.dart
+++ dyce/lib/flappybird/flappy.dart
@@ -16,6 +16,7 @@ import 'package:flutter/services.dart';
 class FlappyGame extends FlameGame
     with HasCollisionDetection, TapDetector, PanDetector {
   late Bird player;
+  late Pipe one;
 
   @override
   Color backgroundColor() => Color.fromARGB(70, 0, 0, 0);
@@ -28,23 +29,83 @@ class FlappyGame extends FlameGame
 
     player = Bird()
       ..size = Vector2(20, 20)
-      ..position = Vector2(size.x / 3, size.y / 2);
+      ..position = Vector2(size.x / 3.5, size.y / 2);
     add(player);
+
+    one = Pipe(30, size.y / 2)
+      ..size = Vector2(30, size.y / 2)
+      ..position = Vector2(size.x / 2, 0);
+
+    add(one);
+  }
+
+  @override
+  void update(double dt) {
+    super.update(dt);
   }
 
   @override
   void onTap() {
+    //print("working");
     player.move();
+  }
+}
+
+class Pipe extends PositionComponent with CollisionCallbacks {
+  double wi = 0;
+  double len = 0;
+  Pipe(double w, double l) {
+    wi = w;
+    len = l;
+    Bound top = Bound(0, 0, w, l, true);
+    add(top);
+
+    Bound bottom = Bound(0, 0, w, l, true);
+  }
+}
+
+class Bound extends PositionComponent with CollisionCallbacks {
+  final _paint = Paint()..color = Color.fromARGB(255, 255, 255, 255);
+
+  late double l;
+  late double t;
+  late double w;
+  late double h;
+  late bool show;
+
+  Bound(double left, double top, double width, double height, bool s) {
+    position = Vector2(left, top);
+    l = left;
+    t = top;
+    w = width;
+    h = height;
+    show = s;
+  }
+
+  //hitbox for the rectangle
+  Future<void> onLoad() async {
+    add(RectangleHitbox(position: Vector2(l, t), size: Vector2(w, h + 10)));
+  }
+
+  @override
+  void render(Canvas canvas) {
+    if (show) {
+      canvas.drawRect(Rect.fromLTWH(l, t, w, h), _paint);
+    }
   }
 }
 
 class Bird extends PositionComponent with CollisionCallbacks {
   final _paint = Paint()..color = Colors.white;
 
-  double jump = 4;
+  double jump = -75;
+  double time = 3;
+
+  bool jumped = false;
 
   void move() {
-    position.y += jump;
+    position.add(Vector2(0, jump));
+    jumped = true;
   }
 
   @override
@@ -52,8 +113,19 @@ class Bird extends PositionComponent with CollisionCallbacks {
     canvas.drawRect(size.toRect(), _paint);
   }
 
+  //hitbox for the rectangle
+  Future<void> onLoad() async {
+    add(RectangleHitbox(size: size));
+  }
+
   @override
   void update(double dt) {
-    position.add(Vector2(0, 3));
+    time += dt;
+    //print(time);
+    position.add(Vector2(0, time / 2.1 * time / 2.1));
+    if (jumped) {
+      time = 3;
+      jumped = false;
+    }
   }
 }
